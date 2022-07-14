1f420d118bb0ac4631fd71d9e56c5a6611d776a4
igor ricci <assis.ricci@gmail.com>
2022-07-14 15:19:58 -0300

    add folder, tiktik logo to navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from 'next/router';
+import { GoogleLogin, GoogleLogout } from 'react-google-login'
+import { AiOutlineLogout } from 'react-icons/ai'
+import { BiSearch } from 'react-icons/bi'
+import { IoMdAdd } from 'react-icons/io'
+
+import Logo from '../utils/tiktik-logo.png'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>
+      <Link href='#'>
+        <Image
+          className='cursor-pointer'
+          src={Logo}
+          alt='TikTik'
+          layout='responsive'
+        />
+      </Link>
+    </div>
   )
 }
 
